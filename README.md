fc0da6b8a2e184a64d4ec5f50444434310a3745f
JoshuaTan <JoshuaTan@users.noreply.github.com>
2022-03-15 10:07:28 +0800

    Created the solve board function
    I was able to finish the project and created a sudoku solver. My next step is to figure out how to create a visualizer for it

--- README.md
+++ README.md
@@ -11,11 +11,14 @@ The input will be a `9 x 9` board. The empty spaces will be marked with `0` and 
 There will a few functions:
 * `isValid()`
 * `findSquare()`
+* `solveBoard()`
 
 ## `isValid()`
 | This method will check if the attempted number is a valid answer. It will check horizontally, vertically, and in the `3x3` box of the coordinate.  
 ## `findSquare()`
 | This method will find a square that is not filled in so we do not overwrite a preexisting/filled square.   
+## `solveBoard()`
+| This method is where the backtracking happens. It will interate through the board and find an empty square. With it, it will try every possible number while checking it is valid. If it works, it will move on to a different square and do the same thing. If it encounters an invalid number and there are no other options, it will then backtrack one square (the previous square) and try the next possible number and continue.   
 
 # Technology 
 This project is created with: 
@@ -25,3 +28,4 @@ This project is created with:
 
 # Improvements
 * I plan on using my code to make an interactive web page. The user is able to solve sudoku puzzles and will also indicate when a certain move is invalid. 
+I will need to make a visualization on how the program works. 
--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,13 @@
-board = [
-    [1,2,3,0,0,0,0,0,0],
-    [4,5,6,0,0,0,0,0,0],
-    [7,8,9,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
+startingBoard = [
+    [0,0,0,5,0,0,0,7,9],
+    [0,0,5,0,7,9,0,4,8],
+    [7,0,8,6,0,4,0,0,0],
+    [0,0,7,0,0,0,0,1,2],
+    [6,0,4,0,0,0,8,0,3],
+    [8,2,0,0,0,0,4,0,0],
+    [0,0,0,7,0,5,3,0,6],
+    [4,7,0,8,3,0,1,0,0],
+    [2,5,0,0,0,1,0,0,0],
 ]
 
 def printBoard(board):
@@ -22,16 +22,33 @@ def printBoard(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
+def solveBoard(board):
+    square = findSquare(board)
+    if not square:
+        return True
+    else:
+        row,col = square
+    # now with an empty square, we will try every possible number
+    for i in range(1,10):
+        if isValid(board,(row,col),i):
+            print("Trying number " + str(i) + " at position: " + str(row) + " "+str(col))
+            board[row][col] = i
+            if solveBoard(board):
+                return True
+            board[row][col]  = 0
+    return False
+
+
+
+
 def isValid(board,coordinate,number):
     #check the row 
     for j in range(len(board[0])):
-        print(board[coordinate[0]][j])
         if board[coordinate[0]][j] == number and coordinate[1] != j:
             return False
 
     #check the col
     for i in range(len(board)):
-        print(board[i][coordinate[1]])
         if board[i][coordinate[1]] == number and coordinate[0] != i:
             return False
 
@@ -39,13 +56,18 @@ def isValid(board,coordinate,number):
     #we need to mod the coordinate in order to know which box it is in
     xBox = coordinate[1]//3
     yBox = coordinate[0]//3
-    print(xBox,yBox)
     for i in range(yBox*3,yBox*3+3): 
         for j in range(xBox*3,xBox*3+3):
             if (board[i][j] == number and (i,j) != coordinate):
                 return False
     return True
 
+def findSquare(board):
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] == 0:
+                return (row,col)    
+    return None
 
 
-
+solveBoard(startingBoard)
