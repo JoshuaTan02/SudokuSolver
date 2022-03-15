719428a02479f9e85499a537c135f8a5a1cafa61
JoshuaTan <JoshuaTan@users.noreply.github.com>
2022-03-15 20:35:34 +0800

    Rewrote txt file
    I had to rewrite the txt file so I could copy and paste it into the js file

--- sudoku_data.py
+++ sudoku_data.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+boards = []
+
 for p in Path('.').rglob('*.dat'):
     # print("New file")
     with p.open() as f:
         board=[]
         lines = 0
+
         for line in f:
             lines+=1
             if lines>2 and lines <12:
@@ -12,14 +15,30 @@ for p in Path('.').rglob('*.dat'):
                 line = line.replace("\n","")
                 newRow = line.split(" ")
                 board.append(newRow)
-            
+        boards.append(board)
         fileName = p.name.replace(".dat",".txt")
-        print(fileName)
-        with open(fileName, 'w') as f:
-            for row in board:
-                for element in row:
-                    f.write(element)
-                
-                f.write(',')
-        
+        # print(fileName)
+        # with open(fileName, 'w') as f:
 
+        #     for row in board:
+        #         for element in row:
+        #             f.write(element)
+                
+        #         f.write(',')
+
+        with open("AllBoards1.txt", 'w') as f:
+            for board in boards:
+                f.write("[")
+                for row in board:
+                    f.write("[")                    
+                    for element in row:
+                        f.write("\"")
+                        f.write(str(element))
+                        f.write("\"")
+                        f.write(',')
+                    f.write("]")                    
+                    f.write(',')
+
+                f.write("]")
+                f.write(',')        
+                f.write("\n")
